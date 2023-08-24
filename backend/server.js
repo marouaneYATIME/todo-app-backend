30287bb7ce6b9bbc91bee74d0696c6e9ce249d42
marouaneYATIME <marwaneyatime14@gmail.com>
2023-08-24 17:29:15 +0200

    deploy app 2

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://todo-list-app-inak-api.onrender.com/"],
+    origin: ["http://localhost:3000", "https://todo-list-app-inak.onrender.com/"],
   })
 );
 
